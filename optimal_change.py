65e3649a87ea13ecac0f8c3ffeb2464cf9f27913
kineticvenom <kalebvarnes@yahoo.com>
2022-06-03 10:59:50 -1000

    Current Solution

--- optimal_change.py
+++ optimal_change.py
@@ -1,10 +1,10 @@
 
 import math
-def optimal_change(item_amount, amount_paid) :
+def optimal_change(item_amount,amount_paid) :
     change_needed = ''
     change_needed = float(amount_paid - item_amount)
-    print((change_needed))
     change_given = ''
+
     money_obj = {
         '$100 bill': 100.00,
         '$50 bill':  50.00,
@@ -24,29 +24,21 @@ def optimal_change(item_amount, amount_paid) :
     for money in money_obj:
         output = ''
         counter = 0
-        # print(money)
         while change_needed >= money_obj[money] :
             counter += 1
             if money in output :      
                 if money == 'penny' :
                     output = str(f'{counter} pennies.')
-                    # change_given += str(f'{counter} pennies.') 
                     change_needed = round((change_needed  - money_obj[money]),2)
-                    
                 else:
-                    output = str(f'{counter} {money}s, ')
-                    # change_given += str(f'{counter} {money}s, ')
+                    output = str(f'{counter} {money}s,')
                     change_needed = round((change_needed  - money_obj[money]),2)
-              
             else :
-                output = str(f'{counter} {money}, ')
-                # change_given += str(f'{counter} {money}, ')
+                output = str(f'{counter} {money},')
                 change_needed = round((change_needed  - money_obj[money]),2)
-        change_given += output   
-    print(change_given)
-        # print(money,money_obj[money])
-    return (f"The optimal change for an item that costs ${item_amount} with an amount paid of ${amount_paid} is {change_given}") #1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
-
-# print("1:", optimal_change(62.13, 100))
-# print("2:", optimal_change(31.51, 50))
-# print("2:", optimal_change(7.50, 5.00))
+                
+        change_given += f"{output} " 
+        total_change =''
+        total_change = total_change.join(change_given) 
+        total_change = (total_change[:-2] +'.')
+    return f"The optimal change for an item that costs ${item_amount} with an amount paid of ${amount_paid} is{total_change}"
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,6 +1,8 @@
-import optimal_change 
+from optimal_change import optimal_change
 
-print("1:", optimal_change(62.13, 100))
-# == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+print("1:", optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
 
-print("2:", optimal_change(31.51, 50)) #== "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+print("2:", (optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."))
+print("3:", optimal_change(42.39, 100)== "The optimal change for an item that costs $42.39 with an amount paid of $100 is 1 $50 bill,   1 $5 bill, 2 $1 bills, 2 quarters, 1 dime,  1 penny.")
+print("4:", optimal_change(72.43, 80) == "The optimal change for an item that costs $72.43 with an amount paid of $80 is    1 $5 bill, 2 $1 bills, 2 quarters,  1 nickle, 2 pennies.")
+print("5:", optimal_change(7.54, 5.00) == "Sorry! You don't have enough money!")
