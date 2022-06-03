54af63d52347d9cf067e65340800b1a95d44f4d8
kineticvenom <kalebvarnes@yahoo.com>
2022-06-03 11:04:08 -1000

    added some sudocode

--- optimal_change.py
+++ optimal_change.py
@@ -4,7 +4,7 @@ def optimal_change(item_amount,amount_paid) :
     change_needed = ''
     change_needed = float(amount_paid - item_amount)
     change_given = ''
-
+#Created a object to store values of money
     money_obj = {
         '$100 bill': 100.00,
         '$50 bill':  50.00,
@@ -17,10 +17,11 @@ def optimal_change(item_amount,amount_paid) :
         'nickle':    0.05,
         'penny':     0.01
     }
-
+#This statement handles if someone cannot afford the purchase
     if change_needed < 0:
         return ("Sorry! You don't have enough money!")
 
+#this loop will cycle through the money object and determine if the change due to custormer needs that specific bill or coin at each step.
     for money in money_obj:
         output = ''
         counter = 0
@@ -38,6 +39,8 @@ def optimal_change(item_amount,amount_paid) :
                 change_needed = round((change_needed  - money_obj[money]),2)
                 
         change_given += f"{output} " 
+
+#This set of code is used to replace any commas left at the end of the sentance with a period.        
         total_change =''
         total_change = total_change.join(change_given) 
         total_change = (total_change[:-2] +'.')
